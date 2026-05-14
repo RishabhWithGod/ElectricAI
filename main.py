6444876dd6a999b866f4377a36701e3c1f2f3c45
Rishabh <rishbh72149@gmail.com>
2026-05-14 21:28:50 +0530

    new command

--- main.py
+++ main.py
@@ -1,8 +1,15 @@
+# from app.main import app
+
+
+# if __name__ == "__main__":
+#     import uvicorn
+#     from app.config import PORT
+
+#     uvicorn.run("main:app", host="0.0.0.0", port=PORT, reload=False)
+import os
+import uvicorn
 from app.main import app
 
-
 if __name__ == "__main__":
-    import uvicorn
-    from app.config import PORT
-
-    uvicorn.run("main:app", host="0.0.0.0", port=PORT, reload=False)
+    port = int(os.environ.get("PORT", 8000))
+    uvicorn.run(app, host="0.0.0.0", port=port)
